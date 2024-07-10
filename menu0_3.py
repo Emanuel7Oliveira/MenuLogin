e1de6ebadc55e26867987c5199790376bfe82ce0
Emanuel <emanuel385@protonmail.com>
2024-07-10 11:54:55 -0300

    Atualização no menu, nova versão.

--- menu0_3.py
+++ menu0_3.py
@@ -1,58 +1,73 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter.ttk import *
 
-def senha():
-    ent = e3.get()
-    if ent == '123':
-        messagebox.showinfo(title='Login aprovado', message=f'Bem vindo, usuário.')
-    else:
-        messagebox.showerror(title='Login ou senha incorretos', message='Falha na autenticação, tente novamente.')
 
+# My func of register login(Already working on)
 def registro():
     nome1 = e1.get()
     sobren1 = e2.get()
     senha1 = e3.get()
+    idade = combo.get()
     
     with open('logon.txt', 'a') as arquivo:
-        arquivo.write(f"\nusuario: {nome1}\nnome completo: {nome1} {sobren1}\nsenha: {senha1}")
+        arquivo.write(f"\nusuario: {nome1}\nnome completo: {nome1} {sobren1}\nsenha: {senha1}\nIdade:{idade}")
     
     messagebox.showinfo(title='Registro concluído', message='Sua conta foi criada com sucesso!', icon='info', detail='Já podes fazer login.')
 
-# creating window, configure window color, window title, window size
+# Creating frame inside window, configuring color, title and size
 class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.pack()
+        self.grid(row=0, column=0, columnspan=1)
         self.funcionamento()
-        self.config(bg='#a3483d')
+        self.config(width=300, height=300, bg='#a3483d')
         
+        self.frames()
+        
+        
+    def frames(self):
+         global combo
+         frame_1 = tk.Frame(self, width=350, height=120,bg='#999999')
+         frame_1.grid(row=0,rowspan=2, column=2)
+         tk.Label(frame_1, text='Selecione sua idade', width=20, height=1, foreground='white', background='black').grid(row=0, column=2)
+         combo = Combobox(frame_1, width=10)
+         combo["values"]= ["14-17", "18-24", "24-34", "35+"]
+         combo.current(0)
+         combo.grid(row=1, column=2, padx=1, pady=10)
+        
+         
     def funcionamento(self):
-        # creating labels
-        tk.Label(self, text='Primeiro nome', width=20, height=1, foreground='white', background='black').grid(row=0, column=0, padx=10, pady=10)
+        
+        tk.Label(self, text='Primeiro nome', width=20, height=1, foreground='white', background='black').grid(row=0, column=0, columnspan=1, padx=10, pady=10)
         tk.Label(self, text='Sobrenome', width=20, height=1, foreground='white', background='black').grid(row=1, column=0, padx=10, pady=10)
         tk.Label(self, text='Senha', width=20, height=1, foreground='white', background='black').grid(row=2, column=0, padx=10, pady=10)
-
+        
         # creating entry boxes
         global e1, e2, e3
         e1 = tk.Entry(self)
         e2 = tk.Entry(self)
-        e3 = tk.Entry(self)
+        e3 = tk.Entry(self, show='*')
         # placing entry boxes
         e1.grid(row=0, column=1, padx=10, pady=10)
         e2.grid(row=1, column=1, padx=10, pady=10)
         e3.grid(row=2, column=1, padx=10, pady=10)
-
+        
         # creating buttons
         button1 = tk.Button(self, text='Sair', width=5, fg="white", bg="black", command=self.master.destroy)
         entrada = tk.Button(self, text='Criar sua conta', width=16, fg="white", bg="black", command=registro)
         # placing buttons
-        button1.grid(row=3, column=1, padx=1, pady=10)
-        entrada.grid(row=3, column=0, padx=10, pady=10)
+        entrada.grid(row=3, column=1, padx=1, pady=10)
+        button1.grid(row=3, column=2, pady=10)
+        
 
+# Creating and configuring window
 root = tk.Tk()
 root.title('Menu de login:')
-root.geometry("700x500")
+root.geometry("1200x500")
 root.config(bg='#a3483d')
+
+# Making magic work
 
 myapp = App(root)
 root.mainloop()
