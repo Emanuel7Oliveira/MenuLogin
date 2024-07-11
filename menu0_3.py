30c72aa664e6cb6f6652fe4a2627379228438ba3
Emanuel <emanuel385@protonmail.com>
2024-07-10 21:59:46 -0300

    Codigo da V0.3 menu atualizado

--- menu0_3.py
+++ menu0_3.py
@@ -9,11 +9,13 @@ def registro():
     sobren1 = e2.get()
     senha1 = e3.get()
     idade = combo.get()
-    
-    with open('logon.txt', 'a') as arquivo:
-        arquivo.write(f"\nusuario: {nome1}\nnome completo: {nome1} {sobren1}\nsenha: {senha1}\nIdade:{idade}")
-    
-    messagebox.showinfo(title='Registro concluído', message='Sua conta foi criada com sucesso!', icon='info', detail='Já podes fazer login.')
+    if senha1 == "" or sobren1 == "" or nome1 == "":
+        messagebox.showerror(title="Erro ao criar login!", message='Falha ao criar conta\nTente novamente.')
+    else:
+        with open('logon.txt', 'a') as arquivo:
+            arquivo.write(f'\nusuario: {nome1}\nnome completo: {nome1} {sobren1}\nsenha: {senha1}\nIdade:{idade}')
+        arquivo.close()
+        messagebox.showinfo(title='Registro concluído', message='Sua conta foi criada com sucesso!', icon='info', detail='Já podes fazer login.')
 
 # Creating frame inside window, configuring color, title and size
 class App(tk.Frame):
@@ -22,7 +24,6 @@ class App(tk.Frame):
         self.grid(row=0, column=0, columnspan=1)
         self.funcionamento()
         self.config(width=300, height=300, bg='#a3483d')
-        
         self.frames()
         
         
@@ -32,13 +33,12 @@ class App(tk.Frame):
          frame_1.grid(row=0,rowspan=2, column=2)
          tk.Label(frame_1, text='Selecione sua idade', width=20, height=1, foreground='white', background='black').grid(row=0, column=2)
          combo = Combobox(frame_1, width=10)
-         combo["values"]= ["14-17", "18-24", "24-34", "35+"]
+         combo["values"]= [" ", "14-17", "18-24", "25-34", "35+"]
          combo.current(0)
          combo.grid(row=1, column=2, padx=1, pady=10)
         
          
     def funcionamento(self):
-        
         tk.Label(self, text='Primeiro nome', width=20, height=1, foreground='white', background='black').grid(row=0, column=0, columnspan=1, padx=10, pady=10)
         tk.Label(self, text='Sobrenome', width=20, height=1, foreground='white', background='black').grid(row=1, column=0, padx=10, pady=10)
         tk.Label(self, text='Senha', width=20, height=1, foreground='white', background='black').grid(row=2, column=0, padx=10, pady=10)
@@ -68,6 +68,5 @@ root.geometry("1200x500")
 root.config(bg='#a3483d')
 
 # Making magic work
-
 myapp = App(root)
 root.mainloop()
